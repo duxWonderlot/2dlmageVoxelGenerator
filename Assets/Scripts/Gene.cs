471cfee083fe8964a2d55cdcff40cc875d35ece8
DevanaboinaSaiVamshi <35272406+duxWonderlot@users.noreply.github.com>
2020-09-05 21:49:03 +0530

    Voxel Generator
    converts 2d image to 3d

---
+++ Assets/Scripts/Gene.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime_Texture
+{
+    public class Gene : MonoBehaviour
+    {
+
+        // public float Offset;
+        [SerializeField]
+        Texture2D heightMap;
+        [Range(0, 10)]
+        public float height_Scale;
+         //[SerializeField]
+        //GameObject obj;
+        [SerializeField]
+        Color[] pixels;
+        [SerializeField]
+        Color color;
+        int i, j;
+        [SerializeField]
+        List<GameObject> collect_objects = new List<GameObject>();
+        int count = 0;
+        [SerializeField]
+        GameObject prefab;
+        [SerializeField]
+        Transform plane;
+        private void Start()
+        {
+
+            pixels = heightMap.GetPixels(0, 0, heightMap.width, heightMap.height);
+
+            for (i = 0; i < heightMap.width; i++)
+            {
+                for (j = 0; j < heightMap.height; j++)
+                {
+                    color = pixels[(i * heightMap.width) + j];
+                    // = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    GameObject obj =Instantiate(prefab);
+                    obj.transform.rotation = plane.transform.rotation;
+                    obj.transform.position = new Vector3(i, color.grayscale * height_Scale, j);
+                    obj.gameObject.transform.SetParent(this.gameObject.transform);
+                    obj.GetComponent<Renderer>().material.color = color;
+                    collect_objects.Add(obj);
+                }
+            }
+        }
+
+         
+    }
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    Vector3 Startpositon;
+    [SerializeField]
+    public float Fraction;
+     
+
+    private void Update()
+    {
+       
+        transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y+ Mathf.Sin(Random.Range(Time.deltaTime, Time.timeSinceLevelLoad))*Time.deltaTime/4, this.gameObject.transform.position.z);
+    }
+}
